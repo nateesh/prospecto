00d7270dd9d5f84dd57f09fa85e6a922a0cce010
nate esh <nathan.eshraghi@gmail.com>
2024-01-16 19:07:42 +1000

    major fix layout styles, add nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,10 @@
 	import { onMount } from 'svelte';
 	import { ModeWatcher, toggleMode } from 'mode-watcher';
 	import { Sun, Moon } from 'radix-icons-svelte';
-	import { Button } from '$lib/components/ui/button';
 	import type { SubmitFunction } from '@sveltejs/kit';
 	import { enhance } from '$app/forms';
+
+	import { Button } from '$lib/components/ui/button';
 
 	export let data;
 
@@ -39,25 +40,36 @@
 </svelte:head>
 
 <ModeWatcher />
-<div class="justify-right">
-	<Button on:click={toggleMode} variant="outline" size="icon" class="justify-right">
-		<Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
-		<Moon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
-		<span class="sr-only">Toggle theme</span>
-	</Button>
-	{#if session != null}
-		<div class="text-right">
-			<h1 class="text-lg font-bold">Welcome, {session.user.email}</h1>
-			<form method="post" action="/account?/signout" use:enhance={handleSignOut}>
-				<Button type="submit" variant="outline" disabled={loading}>Sign Out</Button>
-			</form>
-		</div>
-	{/if}
-</div>
-<div class="container" style="padding: 50px 0 100px 0">
-
-	<slot />
-
+<div class="container">
+	<div class="flex items-stretch h-16">
+    <div class="mr-auto border-2">
+      <h1>Prospecto</h1>
+    </div>
+		<div class="ml-auto flex flex-wrap items-stretch">
+      <Button variant="ghost">Nav</Button>
+      <div>
+        <Button on:click={toggleMode} variant="outline" size="icon" class="">
+          <Sun class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
+          <Moon class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+          <span class="sr-only">Toggle theme</span>
+        </Button>
+      </div>
+      
+      {#if session != null}
+      <Button variant="ghost" href="/account">Account</Button>
+      <Button variant="ghost" href="/dashboard">Dashboard</Button>
+      <h1 class="text-sm font-bold">Welcome, {session.user.email}</h1>
+      <div class="text">
+        <form method="post" action="/account?/signout" use:enhance={handleSignOut}>
+          <Button type="submit" variant="ghost" disabled={loading}>Sign Out</Button>
+        </form>
+      </div>
+      {/if}
+    </div>
+	</div>
+  <div class="h-screen px-5">
+    <slot />
+  </div>
 </div>
 
 <style lang="postcss">
